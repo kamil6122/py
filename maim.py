953b43702fed9aacdac25ce910d7b5d78ea1e23e
kamil6122 <kamil.ruszkiewicz.7@gmail.com>
2024-01-16 23:51:24 +0100

    deletowanie dziala wiec trzeba comita zrobic bo zara sie sp!$@%^li

--- maim.py
+++ maim.py
@@ -1,7 +1,8 @@
 import psycopg2
 from flask import Flask, render_template, request, redirect, url_for
 from flask_wtf import FlaskForm
-from wtforms import FloatField, SubmitField, SelectField
+from wtforms import FloatField, SubmitField, SelectField, RadioField, IntegerField
+from wtforms.validators import InputRequired
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret_key'
@@ -28,51 +29,33 @@ def getquery(sql):
     cursor.close()
     return rows
 
+# FORM CLASSES
 
-class Form(FlaskForm):
+class AddCarForm(FlaskForm):
     engine_displacement = FloatField('Engine displacement')
     max_speed = FloatField('Max speed')
-    type_of_fuel = SelectField('Type', choices=[('diesel', 'diesel'), ('gasoline', 'gasoline'), ('electric', 'electric'),
-                                ('hydrogen', 'hydrogen'), ('LPG', 'LPG'), ('hybrid', 'hybrid')])
-    submit = SubmitField('Submit')
+    type_of_fuel = IntegerField('Type of fuel')
+    # type_of_fuel = SelectField('Type', choices=[(1, '1: diesel'), (2, '2: gasoline'), (3, '3: electric'),
+    #                             (4, '4: hydrogen'), (5, '5: LPG'), (6, '6: hybrid')])
+    submit = SubmitField('Add')
+
+
+class DeleteCarForm(FlaskForm):
+    car_id = IntegerField('Delete id')
+    submit = SubmitField('Delete')
 
 
 def validate_and_add(engine_displacement, max_speed, type_of_fuel):
-    if type(engine_displacement) is not float or type(max_speed) is not float:
-        print("One of values is not number")
-        return False
-    if type_of_fuel not in ['diesel', 'gasoline', 'electric', 'hydrogen', 'LPG', 'hybrid', 'biodiesel']:
-        return False
-    print('INSERT INTO test_table VALUES(DEFAULT, {}, {}, ''{}'')'.format(engine_displacement, max_speed, type_of_fuel))
+    # if type(engine_displacement) is not float or type(max_speed) is not float:
+    #     print("One of values is not number")
+    #     return False
+    # if type(type_of_fuel) is not int:
+    #     return False
     insertquery(
         '''INSERT INTO test_table VALUES(DEFAULT, {}, {}, '{}')'''.format(engine_displacement, max_speed, type_of_fuel))
     return True
 
-
-@app.route('/add', methods=['GET', 'POST'])
-def add():
-    form = Form()
-
-    if form.validate_on_submit():
-        # Access form data using form.name.data and form.age.data
-        engine_displacement = form.engine_displacement.data
-        max_speed = form.max_speed.data
-        type_of_fuel = form.type_of_fuel.data
-
-        result = validate_and_add(engine_displacement, max_speed, type_of_fuel)
-
-        # You can process the form data here (e.g., save to a database)
-        # return redirect(url_for('success', engine_displacement=engine_displacement, max_speed=max_speed, type_of_fuel=type_of_fuel))
-        return redirect(url_for('index'))
-        # return redirect(url_for('success', name=name, age=age))
-
-    return render_template('add.html', form=form)
-
-
-@app.route('/success/<float:engine_displacement>/<float:max_speed>/<type_of_fuel>')
-def success(engine_displacement, max_speed, type_of_fuel):
-    return f'Success!'
-
+# APIS
 
 @app.route('/api/data')
 def get_data():
@@ -105,16 +88,49 @@ def post_data():
 @app.route('/api/data/<string:id>', methods=['DELETE'])
 def delete_row(item_id):
     available_rows = getquery('SELECT id FROM test_table')
-    if item_id not in available_rows:
+    if int(item_id) not in available_rows:
         pass  # ERROR MESSAGE 404
     else:
         insertquery('DELETE FROM test_table WHERE id = {}'.format(item_id))
 
 
+# ROUTES
+
 @app.route('/')
 def index():
     data = get_data()
-    return render_template('index.html', data=data)
+    form = DeleteCarForm()
+    print(form.car_id.data)
+    print(form.validate_on_submit())
+    if form.validate_on_submit():
+        car_id = form.car_id.data
+        delete_row(car_id)
+        print('wee?')
+        return redirect(url_for('delete/' + str(car_id)))
+
+    return render_template('index.html', data=data, form=form)
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    form = AddCarForm()
+    if form.validate_on_submit():
+        engine_displacement = form.engine_displacement.data
+        max_speed = form.max_speed.data
+        type_of_fuel = form.type_of_fuel.data
+        print(engine_displacement, max_speed, type_of_fuel)
+        result = validate_and_add(engine_displacement, max_speed, type_of_fuel)
+
+        return redirect(url_for('index'))
+
+    return render_template('add.html', form=form)
+
+
+@app.route('/delete/<car_id>', methods=["POST"])
+def delete_id(car_id):
+    print('widzisz mnie?')
+    delete_row(car_id)
+    # return redirect(url_for('index'))
+    return render_template('delete.html')
 
 
 if __name__ == '__main__':

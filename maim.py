c631985889bd5b02fb375f3d5f95606abc2ec292
kamil6122 <kamil.ruszkiewicz.7@gmail.com>
2024-01-17 12:23:25 +0100

    jest lepiej

--- maim.py
+++ maim.py
@@ -1,7 +1,7 @@
 import psycopg2
-from flask import Flask, render_template, request, redirect, url_for, abort, jsonify
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 from flask_wtf import FlaskForm
-from wtforms import FloatField, SubmitField, SelectField, RadioField, IntegerField
+from wtforms import FloatField, SubmitField, IntegerField
 from wtforms.validators import InputRequired, NumberRange
 
 app = Flask(__name__)
@@ -31,10 +31,11 @@ def getquery(sql):
 
 # FORM CLASSES
 
+
 class AddCarForm(FlaskForm):
-    engine_displacement = FloatField('Engine displacement', validators=[NumberRange(min=0.1)])
-    max_speed = FloatField('Max speed', validators=[NumberRange(min=0.1)])
-    type_of_fuel = IntegerField('Type of fuel', validators=[NumberRange(min=1, max=6)])
+    engine_displacement = FloatField('Engine displacement', validators=[InputRequired(), NumberRange(min=0.1)])
+    max_speed = FloatField('Max speed', validators=[InputRequired(), NumberRange(min=0.1)])
+    type_of_fuel = IntegerField('Type of fuel', validators=[InputRequired(), NumberRange(min=1, max=6)])
     submit = SubmitField('Add')
 
 
@@ -50,20 +51,28 @@ class ConfirmForm(FlaskForm):
 def add_to_db(engine_displacement, max_speed, type_of_fuel):
     insertquery(
         '''INSERT INTO test_table VALUES(DEFAULT, {}, {}, '{}')'''.format(engine_displacement, max_speed, type_of_fuel))
-    return True
 
 # ERRORS
 
+
 @app.errorhandler(404)
 def resource_not_found(e):
+    # if page:
+    #     return render_template('error400.html'), 404
+    # else:
     return jsonify(error=str(e)), 404
+
 
 @app.errorhandler(400)
 def invalid_data(e):
+    # if page:
+    #     return render_template('error400.html'), 400
+    # else:
     return jsonify(error=str(e)), 400
 # APIS
 
-@app.route('/api/data')
+
+@app.route('/api/data', methods=["GET"])
 def get_data():
     results = getquery('SELECT * FROM test_table;')
     data_list = []
@@ -72,8 +81,8 @@ def get_data():
         data_list.append(
             {
                 "id": id_,
-                "engine_displacement": float(engine_displacement),
-                "max_speed": float(max_speed),
+                "engine_displacement": engine_displacement,
+                "max_speed": max_speed,
                 "type_of_fuel": type_of_fuel
             }
         )
@@ -83,20 +92,26 @@ def get_data():
 @app.route('/api/data', methods=['POST'])
 def post_data():
     data_json = request.get_json()
-    if isinstance(data_json['engine_displacement'], (int, float)) and isinstance(data_json['max_speed'], (int, float)) and type(data_json['type_of_fuel']) is int:
+    if (isinstance(data_json['engine_displacement'], (int, float)) and isinstance(data_json['max_speed'], (int, float))
+            and type(data_json['type_of_fuel']) is int):
         add_to_db(data_json['engine_displacement'], data_json['max_speed'], data_json['type_of_fuel'])
         last_id_query = getquery('SELECT LASTVAL()')
         return_json = {'new_primary_key': last_id_query[0][0]}
         return jsonify(return_json)
     else:
-        abort(400, 'Invalid data')
+        response = jsonify({'message': 'Invalid data'})
+        response.status_code = 400
+        return response
+
 
 @app.route('/api/data/<string:car_id>', methods=['DELETE'])
 def delete_row(car_id):
     q = getquery('SELECT id FROM test_table')
     available_rows = [item[0] for item in q]
     if int(car_id) not in available_rows:
-        abort(404, '''ID doesn't exist''')
+        response = jsonify({'message': 'Record not found'})
+        response.status_code = 404
+        return response
     else:
         insertquery('DELETE FROM test_table WHERE id = {}'.format(car_id))
         return jsonify({'deleted_record_primary_key': car_id})
@@ -109,22 +124,22 @@ def index():
     data = get_data()
     return render_template('index.html', data=data)
 
+
 @app.route('/add', methods=['GET', 'POST'])
 def add():
     form = AddCarForm()
-    # DO ZROBIENIA TE KODZIKI I WGL
+
     if request.method == "POST":
         if form.validate_on_submit():
             engine_displacement = form.engine_displacement.data
             max_speed = form.max_speed.data
             type_of_fuel = form.type_of_fuel.data
-            result = add_to_db(engine_displacement, max_speed, type_of_fuel)
+            add_to_db(engine_displacement, max_speed, type_of_fuel)
             return redirect(url_for('index'))
         else:
-            abort(400, 'Invalid arguments')
+            return render_template('error400.html'), 400
 
     return render_template('add.html', form=form)
-
 
 
 @app.route('/delete/<car_id>', methods=["POST"])
@@ -132,7 +147,7 @@ def delete_id(car_id):
     q = getquery('SELECT id FROM test_table')
     available_rows = [item[0] for item in q]
     if int(car_id) not in available_rows:
-        abort(404, '''ID doesn't exist''')
+        return render_template('error404.html'), 404
     return render_template('delete.html', car_id=car_id)
 
 
@@ -141,10 +156,6 @@ def deleting(car_id):
     delete_row(car_id)
     return redirect(url_for('index'))
 
-# ERROR HANDLER
-# @app.errorhandler(400)
-# def bad_request_error(error):
-#     return render_template('400_error.html'), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
